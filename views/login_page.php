54fcb916f8144707fbf92efa109d37b67814f843
edubia <eduardabbalbino@gmail.com>
2023-04-25 08:49:27 -0300

    Modifying css in login, register and welcome pages

--- views/login_page.php
+++ views/login_page.php
@@ -16,7 +16,7 @@ if (isset($_SESSION["user_id"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/login-register.css">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../css/welcome.css">
@@ -26,55 +26,57 @@ if (isset($_SESSION["user_id"])) {
  
     <div class="container">
 
-        <div class="logo-container"> 
-            <div class="logo-position">
-                <a href="../index.php">
-                    <img class="logo" src="../images/logo.png" alt="logo">
-                </a>
-            </div> 
-        </div>
-
-        <div class="div-form">
-
-            <form action="../login.php" method="POST">
-
-                <h1 class="name-login">Faça o seu login</h1>
-
-                <?php if(isset($_GET['err'])): ?>
+        <div class="main" >           
+            <div class="logo-container"> 
+                <div class="logo-position">
+                    <a href="../index.php">
+                        <img class="logo" src="../images/logo.png" alt="logo">
+                    </a>
+                </div> 
+            </div>
+            
+            <div class="div-form">
+    
+                <form class="form-login" action="../login.php" method="POST">
+    
+                    <h1 class="name-login">Faça o seu login</h1>
+    
+                    <?php if(isset($_GET['err'])): ?>
+                        <div>
+                            <?= $_GET['err'] ?>
+                        </div>
+                    <?php endif ?>
+                    
                     <div>
-                        <?= $_GET['err'] ?>
+                        <input class="input-login" type="text" name="email" id="email" placeholder="E-mail" required>
                     </div>
-                <?php endif ?>
-                
-                <div>
-                    <input class="input-login" type="text" name="email" id="email" placeholder="E-mail" required>
-                </div>
-
-                <div>
-                    <input class="input-login" type="password" name="password" id="senha" minlength="8" maxlength="45" placeholder="Senha" required>
-                </div>
-
-                <div class="div-forgot-password">
-                    <p class="forgot-password">
-                        <a href="">Esqueceu sua senha?</a>
-                    </p>
-                </div>
-
-                <div>
-                    <div class="button">
-                        <button class="btn-login">Entrar</button>
+    
+                    <div>
+                        <input class="input-login" type="password" name="password" id="senha" minlength="8" maxlength="45" placeholder="Senha" required>
                     </div>
-                <div>
-
-                <div class="register">
-                    <p class="p-register">
-                        É novo no free-Commerce? 
-                        <a href="user_register_page.php">Crie sua conta!</a>
-                    </p>
-                </div>
-                   
-            </form>
-
+    
+                    <div class="div-forgot-password">
+                        <p class="forgot-password">
+                            <a href="">Esqueceu sua senha?</a>
+                        </p>
+                    </div>
+    
+                    <div>
+                        <div class="button">
+                            <button class="btn-login">Entrar</button>
+                        </div>
+                    <div>
+    
+                    <div class="register">
+                        <p class="p-register">
+                            É novo no free-Commerce? 
+                            <a href="user_register_page.php">Crie sua conta!</a>
+                        </p>
+                    </div>
+                       
+                </form>
+    
+            </div>
         </div>
 
     </div>
--- views/welcome_page.php
+++ views/welcome_page.php
@@ -21,157 +21,50 @@ if (isset($_SESSION["user_id"])) {
 <!-- link da fonte montserrat do google fonts -->
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="../css/welcome.css">
     <title>free-Commerce</title>
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            text-decoration: none;
-        }
 
-        @media screen and (max-width: 768px) {
-            .larguraheader{
-                width: 100%;
-            }
-
-        }
-        body {
-            background-color: rgb(196, 241, 128);
-        }
-
-        .logohead {
-            position: absolute;
-            width: 55%;
-            height: 55%;
-            margin-left: 10%;
-        }
-
-        .imgheader{
-            position: absolute;
-            width: 150px;
-            height: 90px;
-            margin-left: 10%;
-            top: 2%; 
-        }
-
-        .larguraheader {
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            padding-top: 60px;
-        }
-
-        .larguranomeheader {
-            position: absolute;
-            display: flex;
-            gap: 130px;
-            right: 7%;
-        }
-
-        .nomeheader {
-            display: flex;
-            font-family: 'Montserrat', sans-serif;
-            color: rgb(255, 80, 16);
-            font-size: 20px;
-        }
-
-        .nomemain {
-            display: flex;
-            font-family: 'Montserrat', sans-serif;
-            position: absolute;
-            margin-left: 8%;
-            padding: 20px;
-            bottom: 100px;
-            font-size: 40px;
-        }
-
-        .orange {
-            color: orangered;
-        }
-
-        .black {
-            color: black;
-        }
-
-        .green {
-            color: green
-        }
-
-        .bordaultimonome {
-            position: absolute;
-            border: 1px solid rgb(245, 125, 13);
-            border-radius: 20px;
-            margin-top:35%;
-            margin-left: 9%;
-            width: 400px;
-            background-color: orangered
-        }
-
-        .ultimonomemain {
-            border-radius: 20px;
-            font-family: 'Montserrat', sans-serif;
-            justify-content: center;
-            display: flex;
-            font-size:large;
-            color: white;
-        }
-
-        .imgmain1 {
-            position: absolute;
-            height: 75%;
-            margin-left: 57%;
-            margin-top: 3%;
-        }
-
-        .imgmain2{
-        position: absolute;
-        height: 65%;
-        margin-left: 54%;
-        margin-top: 7%;
-        }
-    </style>
 </head>
 <body>
     <header>
-        
         <div class="logoheader">
-        <img class="imgheader" src="/images/logo.png" alt="">
+            <img class="imgheader" src="/images/logo.png" alt="">
         </div>
-
         
         <div class="larguraheader">
-
-    <nav class="larguranomeheader">
-        <a class="nomeheader" href="">Quem somos?</a>
-        <a class="nomeheader" href="">Serviços</a>
-        <a class="nomeheader" href="login_page.php">Login </a>
-        <a class="nomeheader" href="user_register_page.php">Cadastre-se</a>
-    </nav>
-    </div>
-</div>
+            <nav class="larguranomeheader">
+                <a class="nomeheader" href="">Quem somos?</a>
+                <a class="nomeheader" href="">Serviços</a>
+                <a class="nomeheader" href="login_page.php">Login </a>
+                <a class="nomeheader" href="user_register_page.php">Cadastre-se</a>
+            </nav>
+        </div>
     </header>
+
     <main>
 
-    <div class="larguramain" >
-        
-        <div class="nomemain">
-        <h1> <span class="orange"> Inovando </span> <br clas="alturaimg">
-           <span class="black"> sua forma de </span> <br>
-          <span class="green"> comprar </span> <span class="black"> e </span> <br>
-           <span class="green"> vender. </span> 
-        </h1>
-        </div>
-            
-        <div class="imgmain">
-        <img class="imgmain1" src="/images/camada.png" alt="">
-        <img class="imgmain2" src="/images/senhora.png" alt="">
+        <div class="larguramain" >
+                
+            <div class="nomemain">
+            <h1> <span class="orange"> Inovando </span> <br clas="alturaimg">
+            <span class="black"> sua forma de </span> <br>
+            <span class="green"> comprar </span> <span class="black"> e </span> <br>
+            <span class="green"> vender. </span> 
+            </h1>
+            </div>
+                    
+            <div class="imgmain">
+            <img class="imgmain1" src="/images/camada.png" alt="">
+            <img class="imgmain2" src="/images/senhora.png" alt="">
+            </div>
+
+            <nav class="bordaultimonome">
+            <a class="ultimonomemain" href="/views/user_register_page.php"> <h2> Cadastre-se </h2> </a>
+            </nav>
         </div>
 
-        <nav class="bordaultimonome">
-        <a class="ultimonomemain" href="/views/user_register_page.php"> <h2> Cadastre-se </h2> </a>
-        </nav>
-</div>
     </main>
 </body>
 </html>
+
+        
