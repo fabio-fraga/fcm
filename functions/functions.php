3b38a64fb2a503640dd3ab9b8a8c754ba61d6451
fabio-fraga <fabiomouradefraga@gmail.com>
2023-04-27 23:45:29 -0300

    Fixing problem with empty string in has_only_spaces function

--- functions/functions.php
+++ functions/functions.php
@@ -1,19 +1,23 @@
 <?php
 
 function has_only_spaces($string) {
-  $array = str_split($string);
-  $spaces = 0;
-
-  for ($i = 0; $i < sizeof($array); $i++) {
-    if ($array[$i] == ' ') {
-      $spaces++;
+    if (strlen($string) === 0) {
+        return false;
     }
-  }
 
-  if ($spaces === sizeof($array)) {
-    return true;
-  }
-  return false;
+    $array = str_split($string);
+    $spaces = 0;
+
+    for ($i = 0; $i < sizeof($array); $i++) {
+        if ($array[$i] == ' ') {
+            $spaces++;
+        }
+    }
+
+    if ($spaces === sizeof($array)) {
+        return true;
+    }
+    return false;
 }
 
 ?>
