81076589828fa5e802c9220547551a3a55d0c5f3
edubia <eduardabbalbino@gmail.com>
2023-05-08 22:39:39 -0300

    Adding checkbox and sum of values

--- cart_update.php
+++ cart_update.php
@@ -4,17 +4,37 @@ session_start();
 
 require("database/db.php");
 
-if($_GET["action"] == "diminuir"){
+$product_amount = stmt(
+    prepare: "SELECT CDC_QUANTIDADE FROM FCM_CARRINHO_DE_COMPRAS WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?",
+    execute_array: [$_GET["product_id"], $_SESSION["user_id"]],
+    fetch_object: true
+)->data[0]->CDC_QUANTIDADE;
+
+if ($_GET["action"] == "diminuir" && $_GET["product_id"] && $product_amount > 1) {
     stmt(
         prepare: "UPDATE FCM_CARRINHO_DE_COMPRAS SET CDC_QUANTIDADE = CDC_QUANTIDADE - 1 WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?",
         execute_array: [$_GET["product_id"], $_SESSION["user_id"]]
-    );
-    
+    );    
 }
-if($_GET["action"] == "aumentar"){
+
+if ($_GET["action"] == "aumentar") {
     stmt(
         prepare: "UPDATE FCM_CARRINHO_DE_COMPRAS SET CDC_QUANTIDADE = CDC_QUANTIDADE + 1 WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?",
         execute_array: [$_GET["product_id"], $_SESSION["user_id"]]
+    );
+}
+
+if (isset($_POST["selected"]) && $_POST["selected"] == 0) {
+    stmt(
+        prepare: "UPDATE FCM_CARRINHO_DE_COMPRAS SET CDC_SELECIONADO = 0 WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?",
+        execute_array: [$_POST["product_id"], $_SESSION["user_id"]]
+    );
+}
+
+if (isset($_POST["selected"]) && $_POST["selected"] == 1) {
+    stmt(
+        prepare: "UPDATE FCM_CARRINHO_DE_COMPRAS SET CDC_SELECIONADO = 1 WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?",
+        execute_array: [$_POST["product_id"], $_SESSION["user_id"]]
     );
 }
 
--- views/cart_page.php
+++ views/cart_page.php
@@ -14,6 +14,14 @@ $cart = stmt(
     fetch_object: true
 )->data;
 
+$soma = 0;
+
+foreach ($cart as $product) {
+    if ($product->CDC_SELECIONADO == 1) {
+        $soma += $product->PRO_VALOR * $product->CDC_QUANTIDADE;
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -31,6 +39,7 @@ $cart = stmt(
     <table>
         <thead>
             <tr>
+                <th></th>
                 <th>nome</th>
                 <th>preço</th>
                 <th>quantidade</th>
@@ -40,6 +49,13 @@ $cart = stmt(
         <tbody>
             <?php foreach($cart as $product): ?>
                 <tr>
+                    <td>
+                        <form action="../cart_update.php" method="POST">
+                            <input type="hidden" name="selected" value="<?= $product->CDC_SELECIONADO == 0 ? 1 : 0 ?>">
+                            <input type="hidden" name="product_id" value="<?= $product->PRO_CODIGO ?>">
+                            <input type="checkbox" onclick="this.form.submit()" <?= $product->CDC_SELECIONADO == 1 ? "checked" : '' ?>>
+                        </form>
+                    </td>
                     <td><?= $product->PRO_NOME ?></td>
                     <td><?= $product->PRO_VALOR ?></td>
                     <td>
@@ -55,5 +71,8 @@ $cart = stmt(
             <?php endforeach ?>
         </tbody>
     </table>
+    <footer>
+        <h2>Total: <?= $soma ?></h2>
+    </footer>
 </body>
 </html>
