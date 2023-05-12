f40f8de4d36a37dd090da34f31326d144254899d
fabio-fraga <fabiomouradefraga@gmail.com>
2023-05-12 04:37:28 -0300

    Restricting deletion of products and deleting from the cart

--- product_delete.php
+++ product_delete.php
@@ -1,13 +1,21 @@
 <?php 
 
+session_start();
+
 require("database/db.php");
 
 $product_id = $_GET["product_id"];
 
 stmt("
-    DELETE FROM FCM_PRODUTOS WHERE PRO_CODIGO = ?
+    DELETE FROM FCM_CARRINHO_DE_COMPRAS WHERE CDC_PRO_CODIGO = ? AND CDC_USU_CODIGO = ?
     ",
-    [$product_id]
+    [$product_id, $_SESSION["user_id"]]
+);
+
+stmt("
+    DELETE FROM FCM_PRODUTOS WHERE PRO_CODIGO = ? AND PRO_CMT_CODIGO = ?
+    ",
+    [$product_id, $_SESSION["user_id"]]
 );
 
 header("location: views/products_page.php");
