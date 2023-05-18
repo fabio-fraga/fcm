4cddbfb41a4ed1a3b217889f4ecd8c66d96f9b8f
fabio-fraga <fabiomouradefraga@gmail.com>
2023-05-17 21:39:17 -0300

    Adding account page

--- views/header_page.php
+++ views/header_page.php
@@ -27,6 +27,7 @@
                 <img class="tamanho_img" src="../images/img_home/perfil.png" alt="Perfil">
                 <div class="dropdown-content">
                     <span class="dropdown-username">Olá, <?= $_SESSION["user_name"] ?>!</span>
+                    <a class="dropdown-link" href="account_page.php">Sua conta</a>
                     <a class="dropdown-link" href="profile_page.php?user_id=<?= $_SESSION["user_id"] ?>"> Perfil </a>
                     <a class="dropdown-link" href="seller_page.php?user_id=<?= $_SESSION["user_id"] ?>"> Venda seus produtos</a>
                     <a class="dropdown-link" href="../logout.php"> Sair da conta </a>
