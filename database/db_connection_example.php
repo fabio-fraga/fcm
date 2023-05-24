c94f15163ea4205b332c0a5ae04c3432cc61a09e
fabio-fraga <fabiomouradefraga@gmail.com>
2023-05-24 10:59:11 -0300

    Revert "Changing HTML comment to PHP in database connection template"
    This reverts commit baaf159c10068e9fd72e9af58b36ae9b3d2d1cfc.

--- database/db_connection_example.php
+++ database/db_connection_example.php
@@ -1,6 +1,6 @@
-<?php
+<!-- Após inserir o usuário e senha, salve este arquivo como db_connection.php -->
 
-//Após inserir o usuário e senha, salve este arquivo como db_connection.php
+<?php
 
 //Insira o usuário do seu Banco de Dados
 $user = "";
