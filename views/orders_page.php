f4888c91df4866f4dc2cfe853e0f50f0975a1d19
edubia <eduardabbalbino@gmail.com>
2023-07-01 17:36:34 -0300

    Showing product images, evaluations and link to each product page on the order page

--- views/orders_page.php
+++ views/orders_page.php
@@ -45,10 +45,20 @@ $all_products = stmt(
             <div class="products-container">     
                 <?php foreach ($all_products as $key => $product): ?>
                     <div class="grid-item">
-                        <div class="item-container">
-                            <div class="date">#<?= $key + 1 . ' - ' . date_format(date_create($product->VEN_DATA), "d/m/Y") ." às " . explode(' ', $product->VEN_DATA)[1]?></div>
+                        <div class="item-container" onclick="window.location='product_page.php?product_id=<?= $product->PRO_CODIGO ?>'">
+                            <div class="date">#<?= sizeof($all_products) - $key . ' - ' . date_format(date_create($product->VEN_DATA), "d/m/Y") ." às " . explode(' ', $product->VEN_DATA)[1]?></div>
                             <div class="img-container">
-                                <img class="img-item" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/800px-Placeholder_view_vector.svg.png">
+                                <?php
+                                $product_images = stmt(
+                                    prepare: "
+                                        SELECT * FROM FCM_PRODUTOS_FOTOS
+                                        WHERE PFT_PRO_CODIGO = ?
+                                    ",
+                                    execute_array: [$product->PRO_CODIGO],
+                                    fetch_object: true
+                                )->data;
+                                ?>
+                                <img class="img-item" src="<?= "../" . $product_images[0]->PFT_CAMINHO ?>">
                             </div>
                             <div class="seller">
                                 <span><a href=""><?= $product->CMR_NOME ?></a></span>
@@ -70,11 +80,26 @@ $all_products = stmt(
                             ?>
                             <div class="sales"><?= $product_sales == 0 ? "Nenhuma venda" : $product_sales . " vendido(s)"?></div>
                             <div class="rating">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                <?php
+                                
+                                $product_avg = stmt(
+                                    prepare: "
+                                        SELECT AVG(AVA_NOTA) AS PRO_MEDIA_DE_NOTAS
+                                        FROM FCM_AVALIACOES_DOS_PRODUTOS
+                                        WHERE AVA_PRO_CODIGO = ?
+                                            ",
+                                    execute_array: [$product->PRO_CODIGO],
+                                    fetch_object: true
+                                )->data[0]->PRO_MEDIA_DE_NOTAS; 
+                                
+                                ?>
+                                <?php for($i = 1; $i <= 5; $i++): ?>
+                                    <?php if($i <= $product_avg): ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="#ffca0f" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <?php else: ?>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" stroke="#ffca0f" width="14" height="14" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>
+                                    <?php endif ?>
+                                <?php endfor ?>
                             </div>
                         </div>
                     </div>
