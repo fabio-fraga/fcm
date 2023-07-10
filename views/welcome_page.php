46400f1c8912b37b1518d16d19670ed001bcc894
V1niciusmv <viniciusifpe352@gmail.com>
2023-07-10 05:18:06 -0300

    Adding responsiveness to the welcome and about us pages

--- views/welcome_page.php
+++ views/welcome_page.php
@@ -25,11 +25,11 @@ if (isset($_SESSION["user_id"])) {
 
 </head>
 <body>
-    <header>
+    <div class="larguratop">
         <div class="logoheader">
             <img class="imgheader" src="/images/logo.png" alt="">
         </div>
-        
+
         <div class="larguraheader">
             <nav class="larguranomeheader">
                 <a class="nomeheader" href="aboutwe_page.php">Quem somos?</a>
@@ -38,48 +38,44 @@ if (isset($_SESSION["user_id"])) {
                 <a class="nomeheader" href="user_register_page.php">Cadastre-se</a>
             </nav>
         </div>
-    </header>
+    </div>
 
-    <main>
-
-        <div class="larguramain" >
-                
+        <div class="larguramain" >        
             <div class="nomemain">
-            <h1> <span class="orange"> Inovando </span> <br clas="alturaimg">
-            <span class="black"> sua forma de </span> <br>
-            <span class="green"> comprar </span> <span class="black"> e </span> <br>
-            <span class="green"> vender. </span> 
-            </h1>
+                <h1> <span class="orange"> Inovando </span> <br>
+                    <span class="black"> sua forma</span>  <br>
+                    <span class="green">de comprar</span> <br> 
+                    <span class="black"> <span class="left"> e </span> <span class="green"> vender. </span> </span> <br>
+                </h1>
+                <button class="bordaultimonome">
+                    <a class="ultimonomemain" href="home_page.php"> <h2> Veja os produtos </h2> </a>
+                </button>
             </div>
-                    
+            
             <div class="imgmain">
-            <img class="imgmain1" src="/images/camada.png" alt="">
-            <img class="imgmain2" src="/images/senhora.png" alt="">
+                <img class="imgmain1" src="/images/camada.png" alt="">
+                <img class="imgmain2" src="/images/senhora.png" alt="">
             </div>
 
-            <nav class="bordaultimonome">
-            <a class="ultimonomemain" href="home_page.php"> <h2> Veja os produtos </h2> </a>
-            </nav>
         </div>
 
         <div class="lugardovideo">
 
-    <div class="nomeinfo">
-        <h1> <span class="orange">informações </span> <span class="black">para sua</span> <span class="green">utilidade</span></h1>
-    </div>
+            <div class="nomeinfo">
+                <h1> <span class="orange">informações </span> <span class="black">para sua</span> <span class="green">utilidade</span></h1>
+            </div>
 
-    <div class="nomevideo1">
-        <p> Saiba como ultlizar a plataforma através do nosso tutorial em video <br>
-            Em menos de 4minutos você ira ficar por dentro de tudo, de uma forma <br>
-            rapida, simples, e legal, <strong> Assista e fique por dentro!!!</strong>
-        </p>
-    </div>
+            <div class="nomevideo1">
+                <p> Saiba como ultlizar a plataforma através do nosso tutorial em video,
+                    Em menos de 4minutos você ira ficar por dentro de tudo, de uma forma 
+                    rapida, simples, e legal, <br>
+                    <strong> <span class="black-left"> Assista e fique por dentro de tudo!!! </span> </strong>
+                </p>
+            </div>
 
-    <div class="ajustevideo">
-        <video class="tamanhovideo" src="" controls></video> 
-    </div> 
+            <div class="ajustevideo">
+                <video class="tamanhovideo" src="" controls></video> 
+            </div> 
         </div>
-</main>
-
 </body>
 </html>
